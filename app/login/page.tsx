35ac3d5c24b5d24b311b11cd39202b037671098f
louememandu <ememandulouisa@gmail.com>
2026-05-02 17:53:37 +0100

    feat: create login page UI

---
+++ app/login/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+import Link from "next/link";
+
+export default function Login(){
+    return (
+      <main>
+        <h2>Login</h2>
+
+        <nav>
+            <Link href="/">Home</Link> |{" "}
+            <Link href="/add-expense">Add Expenses</Link> |{" "}
+            <Link href="/expenses">View Expenses</Link> |{" "}
+            <Link href="/login">Login</Link> |{" "}
+            <Link href="/">Signup</Link> |{" "}
+        </nav>
+
+        <form>
+            <div>
+                <p>Email</p>
+                <input type="email" placeholder="example@mail.com"/>
+            </div>
+            <div>
+                <p>Password</p>
+                <input type="password" placeholder="Enter your password"/>
+            </div>
+            <button type="submit">Login</button>
+        </form>
+
+        <p>Don't have an account? <Link href="/">Sign up</Link></p>
+      </main>  
+    );
+}
