5ad4084e83ba319228feb90357b58c866f873f0b
louememandu <ememandulouisa@gmail.com>
2026-05-16 15:03:08 +0100

    refactor: add navbar component across pages

--- app/expenses/page.tsx
+++ app/expenses/page.tsx
@@ -1,19 +1,12 @@
 "use client";
-
-import Link from "next/link";
+import Navbar from "../../components/Navbar";
 
 export default function Expenses(){
     return (
         <main>
             <h2>View Your Expenses</h2>
 
-            <nav>
-                <Link href="/">Home</Link>
-                <Link href="/add-expense">Add Expenses</Link>
-                <Link href="/expenses">View Expenses</Link>
-                <Link href="/login">Login</Link>
-                <Link href="/signup">Signup</Link>
-            </nav>
+            <Navbar/>
 
             <p>No expenses added here yet.</p>
         </main>
