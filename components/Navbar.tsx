4779c2c6cb58d1065e705259f98908e8ea2c22db
louememandu <ememandulouisa@gmail.com>
2026-05-25 03:48:52 +0100

    chore: update navigation bar to sidebar navigation component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,23 +1,45 @@
 import Link from "next/link";
 import Image from "next/image";
+import {
+    FiGrid,
+    FiDollarSign,
+    FiLogOut,
+    FiPlus,
+} from "react-icons/fi";
 
 export default function Navbar(){
     return (
-        <header className="navbar">
-        <div className="left-section">
-            <Image src="/favicon.png" alt="App Logo" width={60} height={60} className="nav-logo"/>
-            <h1 className="nav-text">Expense Tracker App</h1>
-        </div>
+        <aside className="navbar">
+            <div className="logo-section">
+                <Image src="/favicon.png" alt="App Logo" width={60} height={60} className="nav-logo"/>
+                <h1 className="nav-text">Expense Tracker App</h1>
+            </div>
 
-        <nav>
-            <Link href="/">Home</Link>
-            <Link href="/add-expense">Add/Delete Expenses</Link>
-            <Link href="/expenses">View Expenses</Link>
-            <Link href="/login">Login</Link>
-            <Link href="/signup">Signup</Link>
-        </nav>
+            <div className="profile-section">
+                <img src="https://i.pravatar.cc/100" alt="Profile Picture" className="profile-img"/>
+                <h3>User</h3>
+            </div>
 
-        </header>
+            <nav className="sidebar-nav">
+                <Link href="/dashboard" className="sidebar-link active">
+                    <FiGrid/>
+                    <span>Dashboard</span>
+                </Link>
+                <Link href="/add-expense">
+                    <FiDollarSign/>
+                    <span>Add/Delete Expenses</span>
+                </Link>
+                <Link href="/expenses">
+                    <FiPlus/>
+                    <span>View Expenses</span>
+                </Link>
+                <Link href="/login">
+                    <FiLogOut/>
+                    <span>Log out</span>
+                </Link>
+            </nav>
+
+        </aside>
         
     );
 }
