ddab34a5516368ff6f3663b2597c3bcb3aeb21bb
louememandu <ememandulouisa@gmail.com>
2026-05-28 21:08:38 +0100

    feat: build basic expenses managment page UI

--- app/(dashboard)/update-expenses/page.tsx
+++ app/(dashboard)/update-expenses/page.tsx
@@ -1,14 +1,42 @@
 "use client";
 import Navbar from "@/components/Navbar";
+import ExpenseCard from "@/components/ExpenseCard";
+import ExpenseForm from "@/components/ExpenseForm";
 
 export default function AddExpense(){
+    const expenses = [
+        {
+        title: "Groceries",
+        amount: 12000,
+        category: "Food",
+        date: "May 28, 2026",
+        },
+        {
+        title: "Uber Ride",
+        amount: 4500,
+        category: "Transport",
+        date: "May 27, 2026",
+        },
+    ];
+
     return(
         <main>
             <h2>Update Expenses</h2>
 
             <Navbar/>
+            <ExpenseForm/>
 
-            <p>Form to add/delete the expenses to be added later.</p>
+            <section>
+                {expenses.map((expense, index) => (
+                <ExpenseCard
+                    key={index}
+                    title={expense.title}
+                    amount={expense.amount}
+                    category={expense.category}
+                    date={expense.date}
+                />
+                ))}
+            </section>
         </main>
     ); 
 }
